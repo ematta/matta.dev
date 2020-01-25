f8c7a4dd298082f6940a874abbe1962b220e41e8
Enrique Matta-Rodriguez <enrique@matta.dev>
2020-01-24 19:42:27 -0600

    Adding tests (do not work)

---
+++ tests/integration/test_user.py
@@ -0,0 +1,13 @@
+import pytest
+from server.routes.user import routes as user_routes
+from aiohttp import web
+
+async def test_user_name(aiohttp_client, loop):
+    app = web.Application()
+    app.add_routes(user_routes)
+    client = await aiohttp_client(app)
+    resp = await client.get('/users/Enrique')
+    assert resp.status == 200
+    text = await resp.json()
+    data = {"success": True, "user": "Enrique"}
+    assert data == text
