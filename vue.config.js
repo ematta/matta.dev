847f36f6b6eb9fa569fc55286d07d675afb3afb7
Enrique Matta-Rodriguez <enrique@matta.dev>
2020-01-25 17:58:34 -0600

    ES Lint fixes

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,18 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config
-      .entry("app")
+      .entry('app')
       .clear()
-      .add("./ui/main.js")
+      .add('./ui/main.js')
       .end();
     config.resolve.alias
-      .set("@", path.join(__dirname, "./ui"))
+      .set('@', path.join(__dirname, './ui'));
+  },
+  devServer: {
+    public: 'matta.dev',
+    host: '0.0.0.0',
+    disableHostCheck: true
   }
 };
