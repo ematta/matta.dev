8e6f06170348efe7abeb7b8db2d1fd459d5c6b96
Enrique Matta-Rodriguez <enrique@matta.dev>
2020-01-25 12:26:07 -0600

    Pulling out APP_ROOT for accessibility

--- mattadev/utilities/config.py
+++ mattadev/utilities/config.py
@@ -5,8 +5,9 @@ import pytoml
 
 
 AIOHTTP_ENV = os.environ["AIOHTTP_ENV"]
+APP_ROOT = pathlib.Path(__file__).parent.parent.parent
 
 
 def load_config():
-    with open(f"{pathlib.Path(__file__).parent.parent.parent}/{AIOHTTP_ENV}.toml") as f:
+    with open(f"{APP_ROOT}/{AIOHTTP_ENV}.toml") as f:
         return pytoml.load(f)
